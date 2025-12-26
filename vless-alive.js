4676da61885376fa786783e49816e0a495c5cb16
eishare <easonhunt@protonmail.com>
2025-12-26 16:31:34 +0800

    Update vless-alive.js

--- vless-alive.js
+++ vless-alive.js
@@ -1,14 +1,14 @@
 export default {
   // 定义定时任务处理器
   async scheduled(event, env, ctx) {
-    const targetUrl = "https://your-render-domain.com/uuid"; // <-- 请替换成您实际部署的domain/uuid
+    const targetUrl = "https://your-render-domain.com/uuid"; // <-- 替换成实际部署的domain/uuid
 
-    // 使用 HEAD 方法发送请求，只获取标头，不下载内容，资源消耗最低
+    // HEAD
     try {
       await fetch(targetUrl, {
         method: 'HEAD',
         headers: {
-          // 伪装一个User-Agent，提高通过率
+          // User-Agent
           'User-Agent': 'Mozilla/5.0 (RenderKeepAlive/1.0)',
         },
       });
